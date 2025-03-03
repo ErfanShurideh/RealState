0546c6da392c22400ed224fcf30f7e781c0ad06c
MohammadErfan Shurideh <erfan.shurideh@gmail.com>
2025-03-03 17:32:53 +0330

    FeatureSection and DiscoverSection

--- client/src/app/(nondashboard)/landing/DiscoverSection.tsx
+++ client/src/app/(nondashboard)/landing/DiscoverSection.tsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 const containerVariants = {
-  hidden: { opacity: 0, y: 50 },
-  visible: { opacity: 1, y: 0 },
-  transition: { duration: 0.5, staggerChildren: 0.2 },
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+  transition: {  staggerChildren: 0.2 },
 };
 
 const itemVariants = {
@@ -16,16 +16,16 @@ const itemVariants = {
   visible: { opacity: 1, y: 0 },
 };
 
-function FeaturesSection() {
+function DiscoverSection() {
   return (
     <motion.div
       initial="hidden"
       whileInView="visible"
-      viewport={{ once: true }}
+      viewport={{ once: true, amount: 0.8}}
       variants={containerVariants}
-      className="py-24 px-6 sm:px-8 lg:px-12 xl:px-16 bg-white"
+      className="py-12 bg-white mb-16"
     >
-      <div className="max-w-4xl xl:max-w-6xl mx-auto">
+      <div className="max-w-6xl xl:max-w-7xl mx-auto px-6 sm:px-8 lg:px-12 xl:px-16">
         <motion.h2
           variants={itemVariants}
           className="text-3xl font-bold text-center mb-12 w-full sm:w-2/3 mx-auto"
@@ -34,26 +34,28 @@ function FeaturesSection() {
         </motion.h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 lg:gap-12 xl:gap-16">
           {[0, 1, 2].map((index) => (
-            <motion.div variants={itemVariants} key={index}>
+            <motion.div
+              variants={itemVariants}
+              key={index}
+            >
               <FeatureCards
-                imageSrc={`/landing-search${3 - index}.png`}
-                title={
-                  [
-                    "Trustworthy and Verified Listing",
-                    "Browse rental Listing with Ease",
-                    "Simplify Your Rental Search whith advanced",
-                  ][index]
-                }
-                description={
-                  [
-                    "Trustworthy and Verified Listing",
-                    "Browse rental Listing with Ease",
-                    "Simplify Your Rental Search whith advanced",
-                  ][index]
-                }
-                linkText={["Explore", "Search", "Discover"][index]}
-                linkHref={["/explore", "/search", "/discover"][index]}
-              />
+              imageSrc={`/landing-search${3 - index }.png`}
+              title={
+                [
+                  "Trustworthy and Verified Listing",
+                  "Browse rental Listing with Ease",
+                  "Simplify Your Rental Search whith advanced"
+                ][index]
+              }
+              description={
+                [
+                  "Trustworthy and Verified Listing",
+                  "Browse rental Listing with Ease",
+                  "Simplify Your Rental Search whith advanced"
+                ][index]
+              }
+              linkText={["Explore","Search","Discover"][index]}
+              linkHref={["/explore","/search","/discover"][index]}/>
             </motion.div>
           ))}
         </div>
@@ -80,7 +82,7 @@ const FeatureCards = ({
       <Image
         src={imageSrc}
         width={400}
-        height={400}
+        height={300}
         className="w-full h-full object-contain"
         alt="title"
       />
@@ -88,12 +90,11 @@ const FeatureCards = ({
     <h3 className="text-xl font-semibold mb-2">{title}</h3>
     <p className="mb-4">{description}</p>
     <Link
-      href={linkHref}
-      className="inline-block border border-gray-300 rounded px-4 py-2 hover:bg-gray-100"
-      scroll={false}
-    >
-      {linkText}
+    href={linkHref}
+    className="inline-block border border-gray-300 rounded px-4 py-2 hover:bg-gray-100"
+    scroll ={false}>
+    {linkText}
     </Link>
   </div>
 );
-export default FeaturesSection;
+export default DiscoverSection;
